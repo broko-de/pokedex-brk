005e2c1830f4272c0ce032d4d802dcff75e08863
fliquin <josefliquin@gmail.com>
2021-04-27 23:16:31 -0300

    Subida inicial del proyecto

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    const view = `
+        <div class="pokemons">
+            <articule class="pokemon-item">
+                <a href="#/1">
+                    <img src="image.png" alt="image">
+                    <h2>Pokemon bonito</h2>
+                </a>
+            </articule>
+        </div>
+    `;
+    return view;
+}
+
+export default Home;
+
